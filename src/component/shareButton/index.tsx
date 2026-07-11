b6fb077197fb24c3218f28c3b3b2cffebc757337
schoi369 <inspirationchoi@gmail.com>
2026-07-11 22:24:47 +0900

    Cropped preview photo & changed sharebutton preview

--- src/component/shareButton/index.tsx
+++ src/component/shareButton/index.tsx
@@ -44,7 +44,7 @@ export const ShareButton = () => {
                 "//" +
                 window.location.host +
                 baseUrl +
-                "/preview_image.png",
+                "/preview_image.jpg",
               link: {
                 mobileWebUrl:
                   window.location.protocol +
